fe9d7ac6bc3a09465b5697bd00a90731000a39a2
Igboanugwo Collins <munisco12@gmail.com>
2022-01-19 14:31:45 +0100

    working on inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -27,52 +27,58 @@ const Form = () => {
     }
 
     return (
-        <form
-            className='grid w-full place-content-stretch bg-white'
-            style={{ padding: '30px 15px' }}
-            onSubmit={handleSubmit}
-        >
-            <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
-                Financial Calculator
-            </h2>
-            <input
-                type='number'
-                id='capital'
-                className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
-                required
-                value={capital}
-                placeholder='Capital to Invest'
-                onChange={handleCapital}
-            />
-            <input
-                type='number'
-                id='percent'
-                className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
-                required
-                value={percent}
-                onChange={handlePercent}
-                placeholder='Percentage'
-            />
-            <input
-                type='number'
-                id='duration'
-                className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
-                required
-                value={duration}
-                onChange={handleDuration}
-                placeholder='duration'
-            />
+        <div className='flex w-full place-content-stretch bg-white'>
+            <div className='grid'>
+                <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
+                    Financial Calculator
+                </h2>
+                <form
+                    className='grid w-full place-content-stretch'
+                    style={{ padding: '30px 15px' }}
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type='number'
+                        id='capital'
+                        className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
+                        required
+                        value={capital}
+                        placeholder='Capital to Invest'
+                        onChange={handleCapital}
+                    />
+                    <input
+                        type='number'
+                        id='percent'
+                        className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
+                        required
+                        value={percent}
+                        onChange={handlePercent}
+                        placeholder='Percentage'
+                    />
+                    <input
+                        type='number'
+                        id='duration'
+                        className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
+                        required
+                        value={duration}
+                        onChange={handleDuration}
+                        placeholder='duration'
+                    />
 
-            <button
-                className=' rounded-md outline-none  bg-[#0059dd] text-white text-sm'
-                style={{ padding: '.5rem 3rem', lineHeight: 2.5 }}
-            >
-                Calculate
-            </button>
-           
-
-                     
-        </form>
+                    <button
+                        className=' rounded-md outline-none  bg-[#0059dd] text-white text-sm'
+                        style={{ padding: '.5rem 3rem', lineHeight: 2.5 }}
+                    >
+                        Calculate
+                    </button>
+                </form>
+                <div className='gs'>
+                    <h4 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
+                        Sum Total
+                    </h4>
+                </div>
+            </div>
+        </div>
     )
 }
 
