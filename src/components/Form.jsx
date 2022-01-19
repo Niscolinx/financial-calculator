c2967da2b6955c0d932f2dd52827c723a16cebe4
Igboanugwo Collins <munisco12@gmail.com>
2022-01-19 17:09:36 +0100

    working on inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -59,7 +59,7 @@ const Form = () => {
             <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
                 Financial Calculator
             </h2>
-            <div className='flex gap-8'>
+            <div className='grid gap-8'>
                 <form className='grid w-full' onSubmit={handleSubmit}>
                     <input
                         type='number'
