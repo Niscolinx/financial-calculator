76aca4c949696a1269c2f56c4134e3766f678f2d
Igboanugwo Collins <munisco12@gmail.com>
2022-01-21 22:30:21 +0100

    working on trading plan

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -60,7 +60,107 @@ const Form = () => {
             <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
                 Financial Calculator
             </h2>
-            <div className='grid gap-8'>
+            <div className='flex gap-8'>
+                <form className='grid w-full' onSubmit={handleSubmit}>
+                    <input
+                        type='number'
+                        id='capital'
+                        className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
+                        required
+                        value={capital}
+                        placeholder='Capital to Invest'
+                        onChange={handleCapital}
+                    />
+                    <input
+                        type='number'
+                        id='percent'
+                        className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
+                        required
+                        value={percent}
+                        onChange={handlePercent}
+                        placeholder='Percentage'
+                    />
+                    <input
+                        type='number'
+                        id='duration'
+                        className='text-black outline-none text-lg p-1 rounded-md justify-self-stretch placeholder-[#b2b7be] focus:border-black mb-4'
+                        required
+                        value={duration}
+                        onChange={handleDuration}
+                        placeholder='duration'
+                    />
+
+                    <button
+                        className=' rounded-md outline-none  bg-[#0059dd] text-white text-sm'
+                        style={{ padding: '.5rem 3rem', lineHeight: 2.5 }}
+                    >
+                        Calculate
+                    </button>
+                </form>
+                <div className='text-[#1c124d] border-2 rounded-md border-[#ccc] w-full py-4 px-6'>
+                    <h4 className='font-medium text-center text-[#1c124d] mb-4 text-2xl'>
+                        Investment Details
+                    </h4>
+
+                    <p>
+                        Invested Capital:{' '}
+                        <span className='font-bold' className='font-bold'>
+                            ${capital}
+                        </span>
+                    </p>
+                    <p>
+                        Percentage Increase:{' '}
+                        <span className='font-bold'>{percent}</span>
+                    </p>
+
+                    <div className='mt-6'>
+                        <div className='border-t'>
+                          
+                            <p>
+                                Initial Capital:{' '}
+                                <span className='font-bold'>${capital}</span>
+                            </p>
+                            <p>
+                                profit:{' '}
+                                <span className='font-bold'>${profit}</span>
+                            </p>
+                            <p>
+                                Final Capital:{' '}
+                                <span className='font-bold'>
+                                    ${finalCapital}
+                                </span>
+                            </p>
+                        </div>
+                        {details &&
+                            details.map((value, i) => {
+                                return (
+                                    <div className='border-t mt-2 relative' key={i}>
+                                        <span className='absolute right-0 bg-[#1c124d] text-white p-1'>
+                                            {i}
+                                        </span>
+                                        <p>
+                                            Initial Capital:{' '}
+                                            <span className='font-bold'>
+                                                ${value.capital}
+                                            </span>
+                                        </p>
+                                        <p>
+                                            profit:{' '}
+                                            <span className='font-bold'>
+                                                ${value.profit}
+                                            </span>
+                                        </p>
+                                        <p>
+                                            Final Capital:{' '}
+                                            <span className='font-bold'>
+                                                ${value.finalCapital}
+                                            </span>
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                    </div>
+                </div>
                 <form className='grid w-full' onSubmit={handleSubmit}>
                     <input
                         type='number'
