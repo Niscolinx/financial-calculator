ada7dfd1722c25ed3684f625d0166525f8eec694
Igboanugwo Collins <munisco12@gmail.com>
2022-01-22 10:15:04 +0100

    updated domain url

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -60,9 +60,9 @@ const Form = () => {
             <h2 className='font-medium text-center text-[#1c124d] mb-4 text-2xl border-b-2'>
                 Financial Calculator
             </h2>
-        
-            <div className='flex'>
-                <div className='grid'>
+
+            <div className='flex gap-8'>
+                <div className=''>
                     <h4 className='font-medium text-center text-[#1c124d]'>
                         Profit Percentage
                     </h4>
@@ -172,6 +172,9 @@ const Form = () => {
                     </div>
                 </div>
                 <div className='grid'>
+                    <h4 className='font-medium text-center text-[#1c124d]'>
+                        Trading Plan
+                    </h4>
                     <form className='grid w-full' onSubmit={handleSubmit}>
                         <input
                             type='number'
